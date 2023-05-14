2ee59edcd27ce722bc703c013f7c31e8aebc77a5
RadvanyiZita <radvanyi.zita@gmail.com>
2023-05-14 15:30:23 +0200

    ver01

---
+++ amoba.cpp
@@ -0,0 +1,10 @@
+#include "amoba.hpp"
+
+ elem::elem(int x, int y, int H, int W, int state)
+    {
+        x0=x;
+        y0=y;
+        height=H;
+        width=W;
+        allapot=state;
+    }
---
+++ amoba.hpp
@@ -0,0 +1,19 @@
+#ifndef AMOBA_HPP_INCLUDED
+#define AMOBA_HPP_INCLUDED
+
+struct elem{
+protected:
+    int x0,y0,height, width,allapot;
+public:
+   elem(int x, int y, int H, int W, int state);
+};
+
+
+
+
+
+
+
+
+
+#endif // AMOBA_HPP_INCLUDED
