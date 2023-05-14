2e3bdc09fd44a8bb59577339696f5f8032340128
RadvanyiZita <radvanyi.zita@gmail.com>
2023-05-14 13:06:23 +0200

    ver0

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "graphics.hpp"
+#include "szamlalo.hpp"
+#include "kivalaszto.hpp"
+#include <ctime>
+#include <vector>
+#include <iostream>
+#include <fstream>
+using namespace genv;
+using namespace std;
+
+int main()
+{
+    return 0;
+}
